8759dd145978fa831ef58c8b1323c8e7b1617cb9
unknown <steinway.nathan@gmail.com>
2022-01-31 18:59:38 -0600

    local changes to file systems

--- public/assets/js/animals.js
+++ public/assets/js/animals.js
@@ -73,6 +73,29 @@ const handleGetAnimalsSubmit = event => {
   getAnimals(animalObject);
 };
 
+const getZookeepers = (formData = {}) => {
+  let queryUrl = '/api/zookeepers?';
+
+  Object.entries(formData).forEach(([key, value]) => {
+    queryUrl += `${key}=${value}&`;
+  });
+
+  fetch(queryUrl)
+    .then(response => {
+      if (!response.ok) {
+        return alert(`Error: ${response.statusText}`);
+      }
+      return response.json();
+    })
+    .then(zookeeperArr => {
+      console.log(zookeeperArr);
+      printResults(zookeeperArr);
+    });
+};
+
+
+
 $animalForm.addEventListener('submit', handleGetAnimalsSubmit);
 
 getAnimals();
+getZookeepers();
